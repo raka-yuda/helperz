37147775e350ddf0626c7846b156d2dbf3000a76
Raka Yuda Pradipta <raka.yuda.pradipta@gmail.com>
2024-08-11 19:30:21 +0700

    feat: add configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
+import withMDX from '@next/mdx';
+
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  pageExtensions: ['js', 'jsx', 'md', 'mdx']
 };
 
-export default nextConfig;
+export default withMDX({
+  extension: /\.mdx?$/,
+  options: {
+    // Additional MDX options can go here
+  },
+})(nextConfig);
